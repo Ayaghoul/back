092ed0819046b98f9b5bfdf91ef0a2ba91ee4eab
Ayaghoul <eyalghoul@gmail.com>
2025-08-22 19:04:56 +0100

    second commit

--- Routes/orderRoutes.js
+++ Routes/orderRoutes.js
@@ -1,4 +1,4 @@
-/*const express = require("express");
+const express = require("express");
 const router = express.Router();
 const { protect, admin } = require("../middleware/authMiddleware");
 const {
@@ -17,30 +17,6 @@ router.get("/:id", protect, getOrderById);
 router.get("/", protect, admin, getOrders);
 router.put("/:id/status", protect, admin, updateOrderStatus);
 
-module.exports = router;*/
-
-const express = require("express");
-const {
-  addOrder,
-  getOrderById,
-  getMyOrders,
-  getOrders,
-  updateOrderStatus,
-} = require("../controllers/orderController");
-const { protect, admin } = require("../middleware/authMiddleware");
-
-const router = express.Router();
-
-// Créer commande / Voir toutes commandes
-router.route("/").post(protect, addOrder).get(protect, admin, getOrders);
-
-// Voir mes commandes
-router.route("/myorders").get(protect, getMyOrders);
-
-// Voir une commande
-router.route("/:id").get(protect, getOrderById);
-
-// Mettre à jour statut (payé/livré)
-router.route("/:id/status").put(protect, admin, updateOrderStatus);
-
 module.exports = router;
+
+
